904d90cf659826acbfb2af0d41e4bea19595e409
ivanpozdin <ivanllpozdin@gmail.com>
2023-08-30 19:29:43 +0400

    Add simple page layout, make game over checkers

---
+++ script.js
@@ -0,0 +1,78 @@
+const PLAYER_A = "x";
+const PLAYER_B = "o";
+
+class GameBoard {
+  #board = Array(9).fill(null);
+  #cells = [];
+  #freeCells = 9;
+
+  #title = document.querySelector("#title");
+
+  #currentPlayer = PLAYER_A;
+  constructor() {
+    for (let i = 0; i <= 8; i++) {
+      this.#cells.push(document.querySelector(`.cell-${i}`));
+    }
+    this.#addMoveHandlers();
+  }
+
+  #addMoveHandlers() {
+    this.#cells.forEach((cell) =>
+      cell.addEventListener("click", (e) => {
+        if (cell.textContent) {
+          return;
+        }
+        cell.textContent = this.#currentPlayer;
+        this.#board[cell.dataset.cellNumber] = this.#currentPlayer;
+        if (this.isGameOver(this.#board)) {
+          this.#title.textContent = "GAME OVER";
+        }
+        this.#freeCells--;
+        this.#changePlayer();
+      })
+    );
+  }
+  #changePlayer() {
+    this.#currentPlayer =
+      this.#currentPlayer === PLAYER_A ? PLAYER_B : PLAYER_A;
+  }
+
+  isGameOver(board) {
+    return (
+      this.#freeCells === 0 ||
+      this.#doHave3InColumns(this.#board) ||
+      this.#doHave3InRows(this.#board) ||
+      this.#doHave3InDiagonals(this.#board)
+    );
+  }
+
+  #doHave3InRows(board) {
+    for (let i = 0; i <= 6; i += 2) {
+      if (board[i] === board[i + 1] && board[i] === board[i + 2]) {
+        return board[i];
+      }
+    }
+    return null;
+  }
+
+  #doHave3InDiagonals(board) {
+    if (board[0] === board[4] && board[0] === board[8]) {
+      return board[0];
+    }
+    if (board[2] === board[4] && board[2] === board[6]) {
+      return board[2];
+    }
+    return null;
+  }
+
+  #doHave3InColumns(board) {
+    for (let i = 0; i <= 2; i++) {
+      if (board[i] === board[i + 3] && board[i] === board[i + 6]) {
+        return board[i];
+      }
+    }
+    return null;
+  }
+}
+
+new GameBoard();
