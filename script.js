331fde17b5fcb1556d0630d9d530f7f2fd0bc403
ivanpozdin <ivanllpozdin@gmail.com>
2023-08-31 16:08:28 +0400

    Limit game rounds

--- script.js
+++ script.js
@@ -1,19 +1,56 @@
 const PLAYER_A = "x";
 const PLAYER_B = "o";
 
+class Player {
+  #name;
+  #symbol;
+  #score = 0;
+  constructor(name, symbol) {
+    this.#name = name;
+    this.#symbol = symbol;
+  }
+
+  get score() {
+    return this.#score;
+  }
+
+  get symbol() {
+    return this.#symbol;
+  }
+
+  get name() {
+    return this.#name;
+  }
+
+  increaseScore() {
+    this.#score++;
+  }
+
+  resetScore() {
+    this.#score = 0;
+  }
+}
+
 class GameBoard {
   #board = Array(9).fill(null);
   #cells = [];
   #freeCells = 9;
   #restartButton = document.querySelector("#restart-btn");
+  #nextRoundButton = document.getElementById("next-round-btn");
   #title = document.querySelector("#title");
+  #playerA = new Player("PLAYER_A", "X");
+  #playerB = new Player("PLAYER_B", "O");
+  #roundsNumber = 1;
+  #gameOver = false;
+  #roundOver = false;
 
-  #currentPlayer = PLAYER_A;
+  #currentPlayer = this.#playerA;
   constructor() {
     for (let i = 0; i <= 8; i++) {
       this.#cells.push(document.querySelector(`.cell-${i}`));
     }
     this.#addMoveHandlers();
+    this.#handleNextRound();
     this.#handleRestart();
     this.#startHandle();
   }
@@ -26,42 +63,82 @@ class GameBoard {
         cell.textContent = "";
       });
       this.#title.textContent = "TIC TAC TOE";
-      this.#currentPlayer = PLAYER_A;
+      this.#roundsNumber = 1;
+      this.#gameOver = false;
+      this.#roundOver = false;
+      this.#playerA.resetScore();
+      this.#playerB.resetScore();
+      this.#currentPlayer = this.#playerA;
+      this.#showRoundNumber();
+      this.#showScores();
+      document.querySelector(".start-window").classList.remove("hidden");
+    });
+  }
+
+  #handleNextRound() {
+    this.#nextRoundButton.addEventListener("click", (e) => {
+      this.#nextRoundButton.classList.add("hidden");
+      this.#board = Array(9).fill(null);
+      this.#freeCells = 9;
+      this.#cells.forEach((cell) => {
+        cell.textContent = "";
+      });
+      this.#title.textContent = "TIC TAC TOE";
+      this.#roundOver = false;
+      this.#showRoundNumber();
     });
   }
 
   #addMoveHandlers() {
     this.#cells.forEach((cell) =>
       cell.addEventListener("click", (e) => {
-        if (cell.textContent) {
+        if (cell.textContent || this.#gameOver || this.#roundOver) {
           return;
         }
-        cell.textContent = this.#currentPlayer;
+        cell.textContent = this.#currentPlayer.symbol;
         this.#board[cell.dataset.cellNumber] = this.#currentPlayer;
         this.#freeCells--;
-        if (this.#isGameOver(this.#board)) {
+        if (this.#isRoundOver(this.#board)) {
+          this.#nextRoundButton.classList.remove("hidden");
+          this.#roundOver = true;
+          this.#roundsNumber++;
           if (this.#isTie()) {
             this.#title.textContent = "TIE";
           } else {
-            if (this.#currentPlayer === PLAYER_A) {
-              this.#title.textContent = `Player A WON`;
-            } else {
-              this.#title.textContent = `Player B WON`;
+            this.#currentPlayer.increaseScore();
+            this.#showScores();
+            if (this.#isGameOverSituation()) {
+              this.#nextRoundButton.classList.add("hidden");
+              this.#title.textContent = `${this.#currentPlayer.name} WON`;
             }
           }
-          // this.#title.textContent = "GAME OVER";
         }
-
         this.#changePlayer();
       })
     );
   }
-  #changePlayer() {
-    this.#currentPlayer =
-      this.#currentPlayer === PLAYER_A ? PLAYER_B : PLAYER_A;
+
+  #showRoundNumber() {
+    document.getElementById("round-number").textContent = this.#roundsNumber;
   }
 
-  #isGameOver(board) {
+  #isGameOverSituation() {
+    return (
+      this.#roundsNumber >= 3 && this.#playerA.score !== this.#playerB.score
+    );
+  }
+
+  #showScores() {
+    document.getElementById("score-player-a").textContent = this.#playerA.score;
+    document.getElementById("score-player-b").textContent = this.#playerB.score;
+  }
+
+  #changePlayer() {
+    this.#currentPlayer =
+      this.#currentPlayer === this.#playerA ? this.#playerB : this.#playerA;
+  }
+
+  #isRoundOver(board) {
     return (
       this.#freeCells === 0 ||
       this.#doHave3InColumns(this.#board) ||
